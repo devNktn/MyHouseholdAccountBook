9ce02e8406c3774b8f99763304b80a421936eecd
devNktn <nakatani.dev.4634@gmail.com>
2022-05-15 01:01:06 +0900

    Install and set git lfs

---
+++ docker/JavaContainer/kakeibo/src/test/java/dev/nakatani/kakeibo/KakeiboApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nakatani.kakeibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
